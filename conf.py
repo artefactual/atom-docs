bdf73df1bf55e1c8891f37d89809bbee7131d067
Jesús García Crespo <jesus@sevein.com>
2013-08-14 13:28:34 -0700

    Add contents as a master page

--- conf.py
+++ conf.py
@@ -41,7 +41,7 @@ source_suffix = '.txt'
 #source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = 'contents'
 
 # General information about the project.
 project = u'AtoM'
